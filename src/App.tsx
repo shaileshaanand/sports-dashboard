66e73f3e5f17f3a4444b0be0b7009743c57b6cf0
Shailesh Aanand <anaandshailu@gmail.com>
2023-11-04 20:19:49 +0530

    add react-query-devtools

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { MantineProvider } from "@mantine/core";
 import { ModalsProvider } from "@mantine/modals";
 import { Notifications } from "@mantine/notifications";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 import { Router } from "./Router";
 import { theme } from "./theme";
@@ -16,6 +17,7 @@ export default function App() {
         <QueryClientProvider client={new QueryClient()}>
           <Notifications position="top-right" />
           <Router />
+          <ReactQueryDevtools initialIsOpen={false} />
         </QueryClientProvider>
       </ModalsProvider>
     </MantineProvider>
